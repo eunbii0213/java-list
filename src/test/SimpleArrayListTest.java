910e8819d12815dc11d41c6a09a9b35a59a7cc46
eunbii0213 <eunbii0213@naver.com>
2023-03-01 17:28:50 +0900

    test: isEmpty() 테스트 코드 작성

---
+++ src/test/SimpleArrayListTest.java
@@ -0,0 +1,54 @@
+package src.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import src.java.SimpleArrayList;
+
+class SimpleArrayListTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAdd() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void set() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contains() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void indexOf() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+        SimpleArrayList list = new SimpleArrayList();
+        Assertions.assertTrue(list.isEmpty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void remove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testRemove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void clear() {
+    }
+}
