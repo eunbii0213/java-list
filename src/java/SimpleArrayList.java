0622132c22f266495da99c4e8d6ba15ea1c3cb66
eunbii0213 <eunbii0213@naver.com>
2023-03-01 14:21:25 +0900

    feat: add(String value) 구현

---
+++ src/java/SimpleArrayList.java
@@ -0,0 +1,66 @@
+package src.java;
+
+public class SimpleArrayList implements SimpleList {
+    private String[] array;
+
+    public SimpleArrayList(int size) {
+        this.array = new String[size];
+    }
+
+    @Override
+    public boolean add(String value) {
+        int size = array.length - 1;
+        array[size] = value;
+        return true;
+    }
+
+    @Override
+    public void add(int index, String value) {
+
+    }
+
+    @Override
+    public String set(int index, String value) {
+        return null;
+    }
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(String value) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(String value) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean remove(String value) {
+        return false;
+    }
+
+    @Override
+    public String remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
