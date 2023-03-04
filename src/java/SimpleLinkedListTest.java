b1f446e3b9e6cd1ecbfdf38759f6d6433d16c36d
eunbii0213 <eunbii0213@naver.com>
2023-03-04 19:25:21 +0900

    test: add(String value) 테스트 코드 작성

---
+++ src/java/SimpleLinkedListTest.java
@@ -0,0 +1,58 @@
+package src.java;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SimpleLinkedListTest {
+
+    @Test
+    void add() {
+        //given
+        SimpleLinkedList list = new SimpleLinkedList();
+        //when
+        String expected = "firstNode";
+        list.add(expected);
+        //then
+        Assertions.assertEquals(expected, list.get(0));
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void indexOf() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void testRemove() {
+    }
+
+    @Test
+    void clear() {
+    }
+}
