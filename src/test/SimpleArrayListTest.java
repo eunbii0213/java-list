3df3b9431afe951cc5427d55885b9c58ecfac491
eunbii0213 <eunbii0213@naver.com>
2023-03-04 17:13:10 +0900

    test: remove(int index) 테스트 코드 작성

--- src/test/SimpleArrayListTest.java
+++ src/test/SimpleArrayListTest.java
@@ -149,7 +149,24 @@ class SimpleArrayListTest {
     }
 
     @Test
-    void testRemove() {
+    void testRemoveWithIndex() {
+        //given
+        SimpleArrayList actual = new SimpleArrayList();
+        SimpleArrayList expected = new SimpleArrayList();
+
+        //when
+        actual.add("1");
+        actual.add("2");
+        actual.add("3");
+        actual.add("4");
+        actual.remove(1);
+
+        expected.add("1");
+        expected.add("3");
+        expected.add("4");
+
+        //then
+        assertArrayEquals(expected.getArray(), actual.getArray());
     }
 
     @Test
